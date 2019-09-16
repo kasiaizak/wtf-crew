971c2d178def6de18bf1e6f02d07f63b06d7b70e
Kasia Izak <kontakt@kasiaizak.pl>
2019-09-16 11:03:20 +0200

    fix: change in gorups members, fix small css bugs

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,10 @@
 const btn1 = document.querySelector('.nav__list-button--1-js')
 const btn2 = document.querySelector('.nav__list-button--2-js')
 const btn3 = document.querySelector('.nav__list-button--3-js')
-const btn4 = document.querySelector('.nav__list-button--4-js')
 
 const groupOne = document.querySelector('.users-container--one-js');
 const groupTwo = document.querySelector('.users-container--two-js');
 const groupThree = document.querySelector('.users-container--three-js');
-const groupFour = document.querySelector('.users-container--four-js');
 
 const allContainers = document.querySelectorAll(".users-container");
 const allButtons = document.querySelectorAll(".nav__list-button");
@@ -16,19 +14,18 @@ const allButtons = document.querySelectorAll(".nav__list-button");
 btn1.addEventListener("click", () => {slideIn(groupOne, btn1)})
 btn2.addEventListener("click", () => {slideIn(groupTwo, btn2)})
 btn3.addEventListener("click", () => {slideIn(groupThree, btn3)})
-btn4.addEventListener("click", () => {slideIn(groupFour, btn4)})
 
 function slideIn(group, btn) {
     allContainers.forEach(element => {
         if (element.classList.contains('visible')) {
-            element.classList.remove('visible');    
+            element.classList.remove('visible');
             console.log(`Visible ${element.innerHTML}`)
         }
         console.log(element)
     });
     allButtons.forEach(button => {
         if (button.classList.contains('nav__list-button--active')) {
-            button.classList.remove('nav__list-button--active');    
+            button.classList.remove('nav__list-button--active');
         }
     });
     btn.classList.add('nav__list-button--active');
